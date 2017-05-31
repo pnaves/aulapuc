092c28cab5a810f6010aa4d5474ea0560733955f
Pedro Ferreiraa <pnaves@gmail.com>
2017-05-30 22:00:09 -0300

    +id

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace WebApplication1.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "value" + id;
         }
 
         // POST api/values
